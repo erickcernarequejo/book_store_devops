cd27c092c9dc60e0d0d44f78755ec15f18495d89
Erick Edgardo Cerna Requejo <william8178@hotmail.com>
2024-05-30 09:46:12 -0500

    Imprimir variables

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,26 @@
 export DB_NAME=$(aws ssm get-parameter --name "/DB/NAME" --query "Parameter.Value" --output text)
 export DB_USERNAME=$(aws ssm get-parameter --name "/DB/USERNAME" --query "Parameter.Value" --output text)
 export DB_PASSWORD=$(aws ssm get-parameter --name "/DB/PASSWORD" --query "Parameter.Value" --output text)
+
+# Imprimir variables de entorno
+echo "DB_NAME: $DB_NAME"
+echo "DB_USERNAME: $DB_USERNAME"
+echo "DB_PASSWORD: $DB_PASSWORD"
+
+# Eliminar variables de entorno
+unset DB_NAME
+unset DB_USERNAME
+unset DB_PASSWORD
+
+# Verificar que las variables han sido eliminadas
+echo "DB_NAME after unset: $DB_NAME"
+echo "DB_USERNAME after unset: $DB_USERNAME"
+echo "DB_PASSWORD after unset: $DB_PASSWORD"
+
 cd /opt/libros
-sudo nohup java -jar -Dserver.port=80 libros-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo nohup java -jar \
+    -Dserver.port=80 \
+    -DB_NAME=$DB_NAME \
+    -DB_USERNAME=$DB_USERNAME \
+    -DB_PASSWORD=$DB_PASSWORD \
+    libros-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
