51274a37f65c8cd8ba25ac93e254f3dc2ab8ecfb
Erick Edgardo Cerna Requejo <william8178@hotmail.com>
2024-05-30 08:32:37 -0500

    Agregar variables de entorno a script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+export DB_NAME=$(aws ssm get-parameter --name "/DB/NAME" --query "Parameter.Value" --output text)
+export DB_USERNAME=$(aws ssm get-parameter --name "/DB/USERNAME" --query "Parameter.Value" --output text)
+export DB_PASSWORD=$(aws ssm get-parameter --name "/DB/PASSWORD" --query "Parameter.Value" --output text)
 cd /opt/libros
 sudo nohup java -jar -Dserver.port=80 libros-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
